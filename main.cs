730f5d762a0bd8379438474224c62c4853b0d084
Ellis Critchlow <ecritchl@hydra4.eecs.utk.edu>
2018-03-28 18:55:17 -0400

    fixed some compilation issues with main.cs

--- main.cs
+++ main.cs
@@ -3,23 +3,26 @@ using System;
 class classes
 {
 	public static void Main(string[] args){
-		using(StreamReader sr = File.OpenText(args[0])){
-			string s;
-			while((s = sr.ReadLine()) != null)	//parse through file once and find labels
+		var i = "-44";
+		Console.WriteLine(string.format("{0:x}", i));
+		var s
+			using(StreamReader sr = File.OpenText(args[0]))
 			{
-				if(s[s.Length] == ':')
+				while((s = sr.ReadLine()) != null)	//parse through file once and find labels
 				{
-					//add to labels
+					if(s[s.Length] == ':')
+					{
+						//add to labels
+					}
+				}
+				sr.Position = 0;
+				while((s = sr.ReadLine()) != null)	//parse through file once and find labels
+				{
+					if(s[s.Length] != ':')
+					{
+						//do stuff and print
+					}
 				}
 			}
-		}
-		sr.Position = 0;
-		while((s = sr.ReadLine()) != null)	//parse through file once and find labels
-		{
-			if(s[s.Length] != ':')
-			{
-				//add to labels
-			}
-		}
 	}
 }
