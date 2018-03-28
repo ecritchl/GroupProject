3554cda984c03389c75541f39f331aebaf0ea22c
Ellis Critchlow <ecritchl@hydra4.eecs.utk.edu>
2018-03-28 18:13:29 -0400

    added main.cs for basic reading of a file

---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+
+class classes
+{
+	public static void Main(string[] args){
+		using(StreamReader sr = File.OpenText(args[0])){
+			string s;
+			while((s = sr.ReadLine()) != null)	//parse through file once and find labels
+			{
+				if(s[s.Length] == ':')
+				{
+					//add to labels
+				}
+			}
+		}
+	}
+
+}
