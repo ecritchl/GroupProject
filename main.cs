6206df57e28adf83a07d0bd809cf865d454a83b9
Ellis Critchlow <ecritchl@hydra4.eecs.utk.edu>
2018-03-28 20:24:50 -0400

    ready to test main for first pass

--- main.cs
+++ main.cs
@@ -1,28 +1,48 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 
 class classes
 {
 	public static void Main(string[] args){
-		var i = "-44";
-		Console.WriteLine(string.format("{0:x}", i));
-		var s
-			using(StreamReader sr = File.OpenText(args[0]))
+		string s;
+		string []input;
+		Dictionary<string, int> labels = new Dictionary<string, int>();
+
+		using(StreamReader sr = File.OpenText(args[0]))
+		{
+			while((s = sr.ReadLine()) != null)	//first pass through file once and find labels
 			{
-				while((s = sr.ReadLine()) != null)	//parse through file once and find labels
+				if(s[0] != '/' && s[0] != '#')	//ignore comments	
 				{
-					if(s[s.Length] == ':')
+					input = s.Split(' ');
+					if(input[0][input[0].Length] == ':')	//check for label by looking for colon
 					{
-						//add to labels
-					}
-				}
-				sr.Position = 0;
-				while((s = sr.ReadLine()) != null)	//parse through file once and find labels
-				{
-					if(s[s.Length] != ':')
-					{
-						//do stuff and print
+						labels.Add(input[0].Substring(0, input.Length-1), Int32.Parse(input[1])); //add to labels
 					}
 				}
 			}
+			foreach(var i in labels)	//print all labels for error checking
+			{
+				Console.WriteLine($"Label: {i.Key}, Address: {i.Value}");
+			}
+		}
+		using(StreamReader sr = File.OpenText(args[0]))
+		{
+			while((s = sr.ReadLine()) != null)	//first pass through file once and find labels
+			{
+				if(s[0] != '/' && s[0] != '#')	//ignore comments	
+				{
+					input = s.Split(' ');
+					if(input[0][input[0].Length] != ':')	//check for instructions by looking for colon
+					{
+						//call interface function
+					}
+				}
+			}
+		}
 	}
 }
+
+//var i = -44;		//conversion from int to hex
+//Console.WriteLine(i.ToString("X"));
